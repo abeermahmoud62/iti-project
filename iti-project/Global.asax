2c246e823372be779bb0364f6a7026df0b74fd8b
AhmedMojahed <asaad0861@gmail.com>
2020-09-28 23:42:24 +0200

    Initial Commit

---
+++ iti-project/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="iti_project.MvcApplication" Language="C#" %>
---
+++ iti-project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(iti_project.Startup))]
+namespace iti_project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
